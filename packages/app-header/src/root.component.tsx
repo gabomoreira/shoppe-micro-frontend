b3ff5d32020d01d4f7059213f33fdfed248f9e66
Gabriel Moreira <gsm.br2003@gmail.com>
2023-03-08 09:31:38 -0300

    feat: add navigate component

--- packages/app-header/src/root.component.tsx
+++ packages/app-header/src/root.component.tsx
@@ -1,3 +1,11 @@
+// @ts-ignore
+import { Navigate } from '@shoppe/util-ui';
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+    <section>
+      <h1>Eu sou o header!!</h1>
+      <Navigate path="sim">Eu vim do alem kkkkk</Navigate>
+    </section>
+  );
 }
